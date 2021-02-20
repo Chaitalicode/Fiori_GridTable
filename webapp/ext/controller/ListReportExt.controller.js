2298219b05e7640ffb5e1a638f12d1f11a076643
Chaitalicode <chaitalipandya17@gmail.com>
2021-02-20 10:17:02 +0530

    second

---
+++ webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,5 @@
+sap.ui.controller("grdT.zchgrid.ext.controller.ListReportExt", {
+    MyActionManagementOpeartionSet : function(oEvent) { 
+        alert('MyActionManagementOpeartionSet');
+    }
+});
